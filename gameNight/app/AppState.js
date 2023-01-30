56968a451249a3214ee452c64de2233f32c0a645
ThomF <seniorswanky@icloud.com>
2023-01-30 16:33:45 -0700

    c

--- gameNight/app/AppState.js
+++ gameNight/app/AppState.js
@@ -9,11 +9,11 @@ class AppState extends EventEmitter {
   /** @type {import('./Models/Player.js').Player[]} */
   players = [new Player('J')];
 
-// players = [
-//   new Player('Slate Slabrock'),
-//   new Player('Tim'),
-//   new Player('Jeremy')
-// ]
+players = [
+  new Player('Slate Slabrock'),
+  new Player('Tim'),
+  new Player('Jeremy')
+]
 }
 
 export const appState = new Proxy(new AppState(), {
